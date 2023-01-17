0bd1d81ae5fed21489e38dcc867d9a87a9343101
Ignacio <Isaavedrapimentel@gmail.com>
2023-01-16 21:31:04 -0300

    commit

--- Index.js
+++ Index.js
@@ -1,12 +1,19 @@
+
+
 function cambio(){
     let dolares = 745 
     let result;
     let pesos = parseInt(document.getElementById('pesos').value,10);
      
     result = pesos / dolares
-      
+
+    // toFixed permite elegir cantidad de decimales de un resultado (en este caso .2)
+    // ejemplo let number = 5.56789
+    //         let n = number.toFixed (2);
+    //         resultado = 5.57
+    
     let resultado = result.toFixed(2);
-      
-    document.getElementById('resul').innerHTML= resultado;
+    
+    document.getElementById('resul').innerHTML = resultado;
     
     }
